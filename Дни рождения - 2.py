15d579a1d11f01ff61b930b95ff28474f02f395f
Grigory <73838499+gregorey0213@users.noreply.github.com>
2024-01-10 14:09:56 +0600

    Add files via upload

--- Дни рождения - 2.py
+++ Дни рождения - 2.py
@@ -3,18 +3,22 @@ for i in range(int(input())):
     string = input().split()
     name, m, d = string[0], string[-1], string[1]
     if m not in dict1:
-        dict1[m] = [(d, name)]
+        dict1[m] = [(int(d), name)]
     else:
-        dict1[m].append((d, name))
+        dict1[m].append((int(d), name))
 list1 = [input() for i in range(int(input()))]
 for i in list1:
-    
     if i in dict1:
-        print(dict1[i])
         temp = []
-        for j in sorted(dict1[i], key=lambda dict1: dict1[i][0]):
-            temp.append(j[0])
-            temp.append(j[1])
-        print(' '.join(str(i) for i in temp))
+        if len(dict1[i]) != 1:
+            for j in sorted(dict1[i], key=len):
+                temp.append((j[0], j[1]))
+            output = [str(i) for j in sorted(temp, reverse=True) for i in j]
+            print(' '.join(i for i in output[::-1]))
+        else:
+            for j in sorted(dict1[i], key=len):
+                temp.append(j[0])
+                temp.append(j[1])
+            print(' '.join(str(i) for i in temp[::-1]))
     else:
         print()
