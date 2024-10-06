e077bfafcdb7117f525f7b09c71143f6bac17fca
gregorey0213 <gregorey060@gmail.com>
2024-10-06 12:59:32 +0600

    Edited file "Калькулятор"

--- for_git.ps1
+++ for_git.ps1
@@ -1,19 +1,39 @@
 ﻿git init
 git add .
 git status
-$message = Read-Host "Enter text message"
-git commit -m $message
-git status
+function Push {
+    $message = Read-Host "Enter text message"
+    git commit -m $message
+    git status
 
-Write-Host "Press enter to continue"
-$key = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-if ($key.VirtualKeyCode -eq 13) {
+    Write-Host "Press enter to continue"
+    $key = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    if ($key.VirtualKeyCode -eq 13) {
 
-    Write-Host "КREP:   git@github.com:grigoriy-st/YandexLyceum.git"
-    Write-Host "\n\n------UPDATED-----\n\n"
+        Write-Host "REMOTE_REP:   git@github.com:grigoriy-st/YandexLyceum.git"
+        Write-Host "------UPDATED-----"
+        Write-Host "--------------SUCCESS--------------"
+    }
+
+    git push git@github.com:grigoriy-st/YandexLyceum.git
 }
 
-git push git@github.com:grigoriy-st/YandexLyceum.git
+function Pull {
+    Write-Host "LOCAL_REP:   git@github.com:grigoriy-st/YandexLyceum.git"
+    git push git@github.com:grigoriy-st/YandexLyceum.git
+    Write-Host "------UPDATED-----"
+    Write-Host "--------------SUCCESS--------------"
+}
 
-Write-Host "--------------SUCCESS--------------"
+$operation = Read-Host "Enter option:
+1 - push
+2 - pull
+3 - merge"
+switch ($operation) {
+    1 { Push }
+    2 { Pull}
+    3 { Write-Host "merge is not found" }
+    Default {Write-Host "ERROR"}
+}
+
 Start-Sleep -s 4
