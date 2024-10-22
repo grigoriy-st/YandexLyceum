ad306321fb0c0b4463f495a873952a98dbb99be3
gregorey0213 <gregorey060@gmail.com>
2024-10-22 22:07:27 +0600

    Edited go-file Calculator

--- Yandex_lyceum/Golang/main/Calculator.go
+++ Yandex_lyceum/Golang/main/Calculator.go
@@ -4,65 +4,130 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
-func find_elment(seq []string, el string) int {
-	for i, val := range seq {
-		if val == el {
+func ExecuteBinOps(seq []string, pos int, sign string) ([]string, error) {
+	first, _ := strconv.Atoi(seq[pos-1])
+	second, _ := strconv.Atoi(seq[pos+1])
+	TempQ := 0
+
+	switch sign {
+	case "*":
+		TempQ = first * second
+	case "/":
+		if second == 0 {
+			return []string{}, fmt.Errorf("Devision by zero")
+		}
+		TempQ = first / second
+	case "-":
+		TempQ = first - second
+	case "+":
+		TempQ = first + second
+	}
+
+	TempSeq := seq[:pos-1]
+	TempQStr := strconv.Itoa(TempQ)
+	TempSeq = append(seq, TempQStr)
+
+	for _, val := range seq[pos+2:] {
+		TempSeq = append(TempSeq, string(val))
+	}
+	return seq, nil
+}
+
+func ExecuteArifOps(seq []string) (int, error) {
+	// 10 + 5 * 4
+	result := 0
+
+	for {
+		if len(seq) == 1 {
+			break
+		}
+		// Execute high priority operations
+		for i := 0; i < len(seq); i++ {
+			if string(seq[i]) == "*" || string(seq[i]) == "/" {
+				TempSeq, err := ExecuteBinOps(seq, i, string(seq[i]))
+				if err != nil {
+					fmt.Println(err)
+				}
+				seq = TempSeq
+			}
+		}
+		// Execute other operations
+		for i := 0; i < len(seq); i++ {
+			if string(seq[i]) == "+" || string(seq[i]) == "-" {
+				TempSeq, err := ExecuteBinOps(seq, i, string(seq[i]))
+				if err != nil {
+					fmt.Println(err)
+				}
+				seq = TempSeq
+			}
+		}
+	}
+	return -1
+}
+
+func FindRightBrckt(exp []string, startIndex int) int {
+	for i := startIndex; i < len(exp); i++ {
+		if string(exp[i]) == ")" {
 			return i
 		}
 	}
 	return -1
 }
 
-func CalcExp(exp string) string {
-	//result := 0
-	re := regexp.MustCompile(`\d+|[+\-*\/()]`)
-	parts := re.FindAllString(exp, -1)
-
-	for {
-		temp_num := 0
-		index := find_elment(parts, "*")
-		if index != -1 {
-			first, _ := strconv.Atoi(parts[index-1])
-			second, _ := strconv.Atoi(parts[index+1])
-			temp_num = first * second
-			temp_str := strconv.Itoa(temp_num)
-
-			temp_parts := parts[:index-1]
-			temp_parts = append(temp_parts, temp_str)
-			for _, val := range parts[index+2:] {
-				temp_parts = append(temp_parts, val)
-			}
-			parts = temp_parts
+func FindLeftBrckt(exp []string, startIndex int) int {
+	for i := startIndex; i >= 0; i-- {
+		if string(exp[i]) == "(" {
+			return i
 		}
 	}
-	return "e"
+	return -1
 }
 
-func Calc(expression string) (float64, error) {
-	// find ( and ), and solve those expressions
-	// if string.Count
-	// cur_str := expression
+func ProcSlcWthBrckts(exp []string) []string {
+	for {
+		length_seq := len(exp)
+		var half_lenght int = length_seq / 2
 
-	first_index := 0
-	second_index := 0
-	for i, l_val := range expression {
+		IndexLeftBracket := -1
+		IndexRightBracket := -1
 
-		if l_val == '(' {
-			first_index = i
-
-			for j, r_val := range expression {
-				if r_val == ')' {
-					second_index = j
-
-					solve_exp := CalcExp(expression[first_index-1 : second_index])
-					expression = expression[:first_index] + solve_exp + expression[second_index+1:]
-					fmt.Println(expression)
+		for i := 0; i < length_seq; i++ {
+			if string(exp[i]) == "(" {
+				if IndexLeftBracket == -1 || IndexRightBracket == -1 {
+					IndexLeftBracket = i
+				}
+			}
+			if string(exp[i]) == ")" {
+				if IndexLeftBracket != -1 || IndexRightBracket == -1 {
+					IndexRightBracket = i
+					break
 				}
 			}
 		}
+		TempExp := exp[IndexLeftBracket : IndexRightBracket+1]
+		ResultTempExp := ExecuteArifOps(TempExp)
+
 	}
+	return nil
+}
+
+func Calc(expression string) (float64, error) {
+	RightsBracketsInSeq := false // правильный учёт расположения и наличия скобок
+
+	if (strings.Contains(expression, "(") == strings.Contains(expression, ")")) &&
+		strings.Count(expression, "(") > 1 {
+		RightsBracketsInSeq = true
+	}
+	re := regexp.MustCompile(`\d+|[+\-*\/()]`)
+	parts := re.FindAllString(expression, -1)
+
+	if RightsBracketsInSeq {
+		parts = ProcSlcWthBrckts(parts)
+	}
+	fmt.Println(parts)
 	// sequensly solving
 	return 0.0, nil
 }
