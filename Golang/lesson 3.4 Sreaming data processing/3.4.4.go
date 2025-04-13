c010f9c2589561c1d79d1708525167e99a379c8f
Grigoriy <gregorey060@gmail.com>
2025-04-13 18:10:08 +0600

    Add some files

--- Golang/lesson 3.4 Sreaming data processing/3.4.4.go
+++ Golang/lesson 3.4 Sreaming data processing/3.4.4.go
@@ -1,17 +1,61 @@
 package main
 
-func MultiplyPipeline(inputNums ...[]int) int {
+import "fmt"
 
+func MultiplyPipeline(inputNums ...[]int) int {
+	numsChan := NumbersGen(inputNums...)
+	filteredChan := Filter(numsChan)
+
+	return Multiply(filteredChan)
 }
 
-func NumbersGen(nums ...int) <-chan int {
+func NumbersGen(nums ...[]int) <-chan int {
+	out := make(chan int)
 
+	go func() {
+		defer close(out)
+
+		for _, slice := range nums {
+			for _, el := range slice {
+				out <- el
+			}
+		}
+	}()
+	return out
 }
 
 func Filter(in <-chan int) <-chan int {
+	out := make(chan int)
 
+	go func() {
+		defer close(out)
+
+		for num := range in {
+			if num > 0 {
+				out <- num
+			}
+		}
+	}()
+
+	return out
 }
 
 func Multiply(in <-chan int) int {
+	result := 1
 
+	for num := range in {
+		result *= num
+	}
+
+	return result
+}
+
+func main() {
+	nums := [][]int{
+		{1, 2, 3, 4, 5},
+		{1, 2, 1},
+	}
+
+	result := MultiplyPipeline(nums...)
+	fmt.Println(result)
 }
